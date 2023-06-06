e97b604fd04d83d78b7beb0d0e3ea40b624f8143
Allison <121940573+Salas-Allison@users.noreply.github.com>
2023-06-06 22:51:12 +0200

    Implementation index.html, description.html et style.css

--- src/main/java/com/artesanoscosco/blog/web/WebController.java
+++ src/main/java/com/artesanoscosco/blog/web/WebController.java
@@ -9,8 +9,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Controller
 public class WebController {
@@ -31,6 +33,24 @@ public class WebController {
         model.addAttribute("produits", produits);
 
         return "index";
+    }
+/*
+    @GetMapping("/description/{id}")
+    public String description(@PathVariable("id") Long id, Model model) {
+        // Récupérez le produit correspondant à l'ID et passez-le au modèle
+        Produit produit = produitDao.findById(id);
+        model.addAttribute("produit", produit);
+
+        return "description";
+    }
+*/
+    @GetMapping(path="/description/{id}")
+    public String description(Model model, @PathVariable long id) {
+        Optional<Produit> produit = produitDao.findById(id);
+
+        model.addAttribute("produit", produit.get());
+
+        return "description";
     }
 
     @GetMapping(path="/admin")
